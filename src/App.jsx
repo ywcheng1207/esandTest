357328ad04d1ca6f9bd6b61f0d703e9908234276
willy_J <ywj766618@gmail.com>
2024-05-21 15:47:13 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ const workerUrl = 'https://edis.esandcloud.com/efaceid/1.10.0/worker.min.1.10.0.
 
 function App() {
   const esandVerifyConfig = {
-    bizId: encodeURIComponent('2024052018151GF15'),
+    bizId: encodeURIComponent('2024052318151GF15'),
     livingType: encodeURIComponent('1'),
-    returnUrl: encodeURI(window.location.href)
+    returnUrl: `get:${encodeURI(window.location.href)}`
   }
   const esandVerifyLink = `https://edis.esandcloud.com/livingdetection/rpverifyh5/public/livingdetect/getLDTURL?strategy=1&bizId=${esandVerifyConfig.bizId}&livingType=${esandVerifyConfig.livingType}&returnUrl=${esandVerifyConfig.returnUrl}`
 
