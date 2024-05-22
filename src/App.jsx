f85bbdc843c7f0530cc104d1627bece24cb085ee
willy_J <ywj766618@gmail.com>
2024-05-22 11:20:20 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ function App() {
     livingType: '1',
     // returnUrl: `get:${encodeURI(window.location.href)}`
     // returnUrl: 'https://www.baidu.com/'
-    returnUrl: 'https://www.google.com/'
+    // returnUrl: 'https://www.google.com/'
+    returnUrl: `get:https://www.google.com/`
   }
   const esandVerifyLink = `https://edis.esandcloud.com/livingdetection/rpverifyh5/public/livingdetect/getLDTURL?strategy=1&bizId=${esandVerifyConfig.bizId}&livingType=${esandVerifyConfig.livingType}&returnUrl=${esandVerifyConfig.returnUrl}`
 
